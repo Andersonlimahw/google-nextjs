447535d15e02afd8e5d8c6b4f28b561e37a7a130
Anderson Lima <andersonlimahw@gmail.com>
2022-04-23 11:47:04 -0300

    🐞  Fixing mobile ux

--- src/components/Loading/Loading.style.ts
+++ src/components/Loading/Loading.style.ts
@@ -6,7 +6,7 @@ export const Container = styled.div`
     flex-wrap: wrap;
     justify-content: space-between;
     align-items: flex-end;
-    padding: 16px 32px;
+    padding: 16px 32px 176px 16px;
 
     @media(max-width: 1024px) {
         padding: 16px 16px;
@@ -17,4 +17,5 @@ export const ContainerItem = styled.div`
     flex-grow: 1;
     padding: 16px 0;
     min-width: 100%;
+    margin-bottom: 48px;
 `;
--- src/components/Logo/Logo.style.ts
+++ src/components/Logo/Logo.style.ts
@@ -23,18 +23,6 @@ export const LogoStyled = styled.h1`
     color: #f8f8f8;
     margin: 8px auto 0 auto;
     animation: ease-in 0.5ms linear;
-    &:hover {
-        background: linear-gradient(-270deg, #f0f0f0 0%, #96B566 50%, #f0f0f0 100%);
-        -webkit-background-clip: text;
-        -webkit-text-fill-color: transparent;
-        &  h3 {
-            &:hover {
-                background: linear-gradient(-270deg, #fff 0%, #96B566 40%, #f0f0f0 100%);
-                -webkit-background-clip: text;
-                -webkit-text-fill-color: transparent;
-            }
-        }
-    }
 `;
 
 export const LogoDescriptionStyled = styled.h3`
